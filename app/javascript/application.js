68e75456357f211fea1a414d89b2a16130a0719c
Miguel Acero <miguelacero528@gmail.com>
2024-02-01 11:18:59 -0500

    quick fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "src"
+import "src/confirm"
+import "src/map"
+import "src/turbo"
--- app/javascript/controllers/onboarding/welcome_controller.js
+++ app/javascript/controllers/onboarding/welcome_controller.js
@@ -2,19 +2,34 @@ import { Controller } from "@hotwired/stimulus"
 import { computePosition, autoUpdate, flip, shift, offset } from "@floating-ui/dom"
 
 export default class extends Controller {
-  static targets = ["reference", "floating"]
-
-  #submitDisabled = true
+  static targets = ["container", "checkbox", "reference", "floating", "submit"]
 
   connect() {
-    console.log(this.referenceTargets)
-    console.log(this.floatingTargets)
   }
 
   toggleVisited(event) {
     const labelContainer = this.referenceTargets.find((elem) => elem.contains(event.target))
     if (labelContainer && event.target.tagName === "A") {
       labelContainer.dataset.visited = "true"
+    }
+  }
+
+  toggleChecked() {
+    const unvisitedElems = this.element.querySelectorAll("[data-visited='false']")
+    const checkedElems = this.checkboxTargets.filter((elem) => elem.checked)
+    const disclosuresAccepted = checkedElems.length === this.containerTargets.length
+    const visitedLinks = unvisitedElems.length === 0
+
+    if (visitedLinks && disclosuresAccepted) {
+      this.submitTarget.disabled = false
+    } else if (!visitedLinks || !disclosuresAccepted) {
+      this.submitTarget.disabled = true
+    }
+
+    if (disclosuresAccepted) {
+      unvisitedElems.forEach((elem) => {
+        elem.classList.add("error-text")
+      })
     }
   }
 }
